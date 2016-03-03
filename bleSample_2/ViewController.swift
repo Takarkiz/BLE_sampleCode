879f8c2546ff006fbd0a20228224498208fe915e
Takarkiz <ponpontanuki16@gmail.com>
2016-03-03 17:31:46 +0900

    Initial Commit

---
+++ bleSample_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bleSample_2
+//
+//  Created by 澤田昂明 on 2016/03/03.
+//  Copyright © 2016年 Takarki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
