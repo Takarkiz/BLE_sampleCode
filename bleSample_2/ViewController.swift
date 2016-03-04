166b580765aa2c45e3cbf58c9b8a22b69c8ad950
Takarkiz <ponpontanuki16@gmail.com>
2016-03-04 11:50:42 +0900

    first commit.BLE端末を検知するまで

--- bleSample_2/ViewController.swift
+++ bleSample_2/ViewController.swift
@@ -6,19 +6,66 @@
 //  Copyright © 2016年 Takarki. All rights reserved.
 //
 
+import CoreBluetooth
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CBCentralManagerDelegate {
+    
+    var myCentralManager:CBCentralManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //CBCentralManagerの状態が変化した時に呼ばれるメソッド
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        print("state \(central.state)");
+        switch (central.state) {
+        case .PoweredOff:
+            print("Bluetoothの電源がOff")
+        case .PoweredOn:
+            print("Bluetoothの電源はOn")
+            // BLEデバイスの検出を開始.
+            myCentralManager.scanForPeripheralsWithServices(nil, options: nil)
+        case .Resetting:
+            print("レスティング状態")
+        case .Unauthorized:
+            print("非認証状態")
+        case .Unknown:
+            print("不明")
+        case .Unsupported:
+            print("非対応")
+        }
+    }
+    
+    /*
+    BLEデバイスが検出された際に呼び出される.
+    */
+    func centralManager(central: CBCentralManager,didDiscoverPeripheral peripheral: CBPeripheral,advertisementData: [String:AnyObject],RSSI: NSNumber){
+        print("pheripheral.name: \(peripheral.name)")
+        print("advertisementData:\(advertisementData)")
+        print("RSSI: \(RSSI)")
+        print("peripheral.identifier.UUIDString: \(peripheral.identifier.UUIDString)")
+        
+        var name: NSString? = advertisementData["kCBAdvDataLocalName"] as? NSString
+        if (name == nil) {
+            name = "no name";
+        }
+        
+     }
+    
+    @IBAction func scanstart(){
+        myCentralManager = CBCentralManager(delegate:self,queue: nil, options:nil)
+    }
+
 
 
 }
